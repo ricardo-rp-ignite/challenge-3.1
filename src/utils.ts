27dc66125223a057ea9c3cd2237183c2124c388a
Moro <ricardoromero4444@gmail.com>
2022-02-22 20:27:09 -0300

    ✨ : Create formatPostDate util

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export function formatPostDate(date: string): string {
+  return new Date(date).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  });
+}
