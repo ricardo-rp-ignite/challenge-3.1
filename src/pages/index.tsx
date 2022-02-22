cb1fb06c161019a0104f4a7f8ff5ce9a50b51c04
Moro <ricardoromero4444@gmail.com>
2022-02-22 20:27:38 -0300

    ✨ : Query posts in homepage static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { formatPostDate } from '../utils';
 
 interface Post {
   uid?: string;
@@ -24,13 +26,28 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return <h1>hello</h1>;
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'posts')
+  );
+
+  const posts = postsResponse.results.map(
+    (post): Post => ({
+      uid: post.uid,
+      first_publication_date: formatPostDate(post.last_publication_date),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    })
+  );
+
+  return { props: { posts } };
+};
