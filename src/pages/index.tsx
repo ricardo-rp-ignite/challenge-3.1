de4ba484dc896ce3254982884ca2c716b7fab143
moro <ricardoromero4444@gmail.com>
2022-03-31 22:31:25 -0300

    ♻️ : Remove redundant state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,37 +28,30 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-function postsMapper({
-  uid,
-  first_publication_date: date,
-  data,
-}: Post): JSX.Element {
-  return <PostLink key={uid} {...{ date, uid, ...data }} />;
-}
-
 const Home: NextPage<HomeProps> = ({
   postsPagination: { results, next_page },
 }) => {
   const [nextPageUrl, setNextPageLink] = useState<string | null>(next_page);
-  const [morePosts, setMorePosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[]>(results);
 
   function fetchMorePosts(): void {
-    if (nextPageUrl !== null)
-      fetch(nextPageUrl)
-        .then(resp => resp.json())
-        .then(data => {
-          setNextPageLink(data.next_page);
-          setMorePosts([...morePosts, ...data.results]);
-        });
+    if (nextPageUrl === null) return;
+
+    fetch(nextPageUrl)
+      .then(resp => resp.json())
+      .then(data => {
+        setNextPageLink(data.next_page);
+        setPosts([...posts, ...data.results]);
+      });
   }
 
   return (
     <main>
       <img src="/images/logo.svg" alt="logo" />
 
-      {results.map(postsMapper)}
-
-      {morePosts.map(postsMapper)}
+      {posts.map(({ uid, first_publication_date: date, data }) => (
+        <PostLink key={uid} {...{ date, uid, ...data }} />
+      ))}
 
       {nextPageUrl !== null && (
         <button type="button" onClick={fetchMorePosts}>
